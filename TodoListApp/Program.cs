d29a966f5eeb6157c8e4b81f61eb29dfa5efa2a2
yushin123321 <fortnaiter0208@gmail.com>
2026-09-16 15:38:01 +0300

    lesson2: create project

---
+++ TodoListApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
